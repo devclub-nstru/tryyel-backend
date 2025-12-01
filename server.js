ef6c5b54d0f15450857fd3e545b2edc67e101291
Rishik <rishik3555@gmail.com>
2025-12-01 17:19:24 +0530

    fixed cors

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000", // Your frontend URL
+    origin: "https://tryyel.vercel.app", // Your frontend URL
     credentials: true,
   })
 );
